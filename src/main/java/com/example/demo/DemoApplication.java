f8ba48d87f351c8bc4dda6495a4940b7c554598f
xupeng <xp123@demo.com>
2020-06-04 17:15:51 +0800

    first

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import net.hasor.spring.boot.EnableHasor;
+import net.hasor.spring.boot.EnableHasorWeb;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableHasor()
+@EnableHasorWeb()
+@SpringBootApplication(scanBasePackages = { "com.example" })
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
